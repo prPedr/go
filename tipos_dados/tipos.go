81e82ba4b4d370683d7159ad51c01643ad62fec7
prPedr <prpedro025@gmail.com>
2026-05-05 04:38:38 -0300

    Tipos de dados: Parte 1

---
+++ tipos_dados/tipos.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// string
+// bool
+// int -> int8, int16, int64 | uint8, uint16, uint64
+// float32 | float64
+// Bytes | Runes
+
+func main() {
+	var nome = "Pedro"
+	var isAdmin = true
+	idade := 21
+
+	fmt.Println(nome)
+	fmt.Println(isAdmin)
+	fmt.Println(idade)
+}
