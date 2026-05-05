ab3c39f1f3a0cb9bad06a78778ee5645ba2413a0
prPedr <prpedro025@gmail.com>
2026-05-05 04:20:25 -0300

    Tipos de variaveis const

--- variaveis/variaveis.go
+++ variaveis/variaveis.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
-// var -> Podem ter seus valores mudados
-
 func main() {
+	// var -> Podem ter seus valores mudados
 	var nome = "Carlos"
 	var idade = 30
 
 	fmt.Printf("%s possuiu %d anos de idade. \n", nome, idade)
 
-	var teste_frase string // Declarando uma variavel e atribuindo um valor a ela
+	// Declarando uma variavel e atribuindo um tipo de dado a ela (So é permitido em var em const nao pode)
+	var teste_frase string
 
 	teste_frase = "Teste de escita"
 
 	fmt.Println(teste_frase)
+
+	// const -> Nao podem ter seus valores alterados
+
+	const pi = 3.14
+	fmt.Printf("Valor de PI: %.2f", pi)
+
+	const url = "https://appowl.com.br"
+	fmt.Println(url)
 }
