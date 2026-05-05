895a2ee8643d32e0f07e434ff966b43dd4910e74
prPedr <prpedro025@gmail.com>
2026-05-05 20:07:16 -0300

    Aprendendo sobre operadores aritimeticos

---
+++ operadores_aritimeticos/operadores.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	valor1 := 10
+	valor2 := 3
+
+	soma := valor1 + valor2
+	subtracao := valor1 - valor2
+	multiplicacao := valor1 * valor2
+	divisao := valor1 / valor2
+	resto := valor1 % valor2
+
+	fmt.Println(soma)
+	fmt.Println(subtracao)
+	fmt.Println(multiplicacao)
+	fmt.Println(divisao)
+	fmt.Println(resto)
+}
