e4b8985ecc4694356e46d6aa745b2524cda919c9
prPedr <prpedro025@gmail.com>
2026-06-09 19:50:22 -0300

    implementando strings em slices

--- slices/slices.go
+++ slices/slices.go
@@ -8,4 +8,12 @@ func main() {
 	lista = append(lista, 10, 20, 30, 40, 50)
 
 	fmt.Println(lista)
+
+	// Criacao do Slices de frutas.
+	frutas := []string{"maca, banana, mamao"}
+	fmt.Println(frutas)
+
+	// Slices de fruta apos o append
+	frutas = append(frutas, "uva, morango")
+	fmt.Println(frutas)
 }
