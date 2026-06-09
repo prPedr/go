66d1f824e6eef7d41688822bd5d7f64939e5c6fd
prPedr <prpedro025@gmail.com>
2026-06-09 09:50:16 -0300

    Operadores de comparacao do tamanho de caracteres do nomes e comparacao de nomes

--- operadores_relacionais/operadores.go
+++ operadores_relacionais/operadores.go
@@ -3,7 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	compararNumeros()
+	for {
+		fmt.Println("|------------------------------------|")
+		fmt.Println("|          Escolha uma opcao         |")
+		fmt.Println("|------------------------------------|")
+		fmt.Println("| 1 - Comparacao de Números          |")
+		fmt.Println("| 2 - Comparacao de tamanho de nomes |")
+		fmt.Println("| 3 - Comparacao de nomes            |")
+		fmt.Println("| 4 - Sair                           |")
+		fmt.Println("|------------------------------------|")
+		fmt.Println()
+
+		var opcao string
+		fmt.Print("Escolha uma opcao: ")
+		fmt.Scan(&opcao)
+
+		switch opcao {
+		case "1":
+			compararNumeros()
+
+		case "2":
+			compararTamanhoNomes()
+
+		case "3":
+			compararNomes()
+
+		case "4":
+			fmt.Println("Finalizando o programa")
+			return
+
+		default:
+			fmt.Println("Opcao invalida.")
+		}
+	}
 }
 
 func compararNumeros() {
@@ -30,5 +62,51 @@ func compararNumeros() {
 
 	if numero1 != numero2 {
 		fmt.Printf("O número 1: %.2f é diferente do número 2: %.2f \n", numero1, numero2)
+	}
+}
+
+func compararTamanhoNomes() {
+	var nome1 string
+	var nome2 string
+
+	fmt.Print("Digite um nome: ")
+	fmt.Scan(&nome1)
+
+	fmt.Print("Digite outro nome: ")
+	fmt.Scan(&nome2)
+
+	if len(nome1) > len(nome2) {
+		fmt.Printf("O número de caracters do nome 1 é maior que o do nome 2 \n")
+	}
+
+	if len(nome1) < len(nome2) {
+		fmt.Printf("O número de caracters do nome 1 é menor que o do nome 2 \n")
+	}
+
+	if len(nome1) == len(nome2) {
+		fmt.Printf("O tamanho de caracters do nome 1 é igual que o do nome 2 \n")
+	}
+
+	if len(nome1) != len(nome2) {
+		fmt.Printf("O tamanho de caracters do nome 1 é maior que o do nome 2 \n")
+	}
+}
+
+func compararNomes() {
+	var nome1 string
+	var nome2 string
+
+	fmt.Print("Digite um nome: ")
+	fmt.Scan(&nome1)
+
+	fmt.Print("Digite outro nome: ")
+	fmt.Scan(&nome2)
+
+	if nome1 == nome2 {
+		fmt.Printf("O nome 1: %s é igual ao nome 2: %s \n", nome1, nome2)
+	}
+
+	if nome1 != nome2 {
+		fmt.Printf("O nome 1: %s é diferente do nome 2: %s \n", nome1, nome2)
 	}
 }
