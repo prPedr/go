b56f1dd7600ea968caa046bba4c5c24ae2983289
prPedr <prpedro025@gmail.com>
2026-06-09 18:27:59 -0300

    Estrutura de loop for

---
+++ loop/loop.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 0; i <= 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	numero := 0
+
+	for numero < 5 {
+		numero++
+		fmt.Print(numero, "\n")
+	}
+}
