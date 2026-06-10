fe7c851f28f727411d6768f96c694fdde6c609d5
prPedr <prpedro025@gmail.com>
2026-06-10 17:34:51 -0300

    Sistema de login simples utilizando funcoes

--- funcoes/funcoes.go
+++ funcoes/funcoes.go
@@ -1,11 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func boasVindas(nome string) {
-	fmt.Printf("Bem vindo de volta %s", nome)
+func somarNumeros() float64 {
+	var numero1, numero2 float64
+
+	fmt.Print("Informe o primeiro número: ")
+	fmt.Scan(&numero1)
+
+	fmt.Print("Informe o segundo número: ")
+	fmt.Scan(&numero2)
+
+	soma := numero1 + numero2
+
+	fmt.Printf("%.2f + %.2f = %.2f", numero1, numero2, soma)
+
+	return soma
+}
+
+type Usuario struct {
+	Nome  string
+	Senha string
+}
+
+func loginSistema() (bool, error) {
+	usuario1 := Usuario{Nome: "Pedro.Cardoso", Senha: "Pedro@123"}
+
+	var nomeInput string
+	var senhaInput string
+
+	fmt.Print("Digite o seu usuario: ")
+	fmt.Scan(&nomeInput)
+
+	fmt.Print("Digite sua senha: ")
+	fmt.Scan(&senhaInput)
+
+	if usuario1.Nome != nomeInput {
+		return false, errors.New("Usuario invalido")
+	}
+
+	if usuario1.Senha != senhaInput {
+		return false, errors.New("Senha invalida")
+	}
+
+	return true, nil
 }
 
 func main() {
-	boasVindas("Pedro")
+	logado, err := loginSistema()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if logado {
+		fmt.Println("Usuario logado")
+	}
 }
