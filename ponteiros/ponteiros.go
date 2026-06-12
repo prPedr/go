e2bf42c19dba289cbe57e50bfbd2780a3713c25c
prPedr <prpedro025@gmail.com>
2026-06-12 13:58:44 -0300

    Alterando valores de um ponteiro

--- ponteiros/ponteiros.go
+++ ponteiros/ponteiros.go
@@ -2,11 +2,28 @@ package main
 
 import "fmt"
 
-func main() {
+func declarandoUmPonteiro() {
 	numero := 60
 
 	var p *int = &numero
 
 	fmt.Println("Valor do ponteiro na memoria P: ", p)
 	fmt.Println("Valor apontado por P: ", *p)
+}
+
+func somarValores(num *int) {
+	*num = 16
+}
+
+func alterandoValoresPonteiros() {
+	numero := 10
+	fmt.Println("Valor inicial da variavel: ", numero)
+
+	somarValores(&numero)
+
+	fmt.Println("Valor atual da variavel numero: ", numero)
+}
+
+func main() {
+	alterandoValoresPonteiros()
 }
